a468a18f8711ff50c2b4c6bfe2f0501a460ff96e
tarehiko <tt1984nucc@gmail.com>
2023-07-18 02:02:10 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@aws-amplify/ui-react/styles.css";
+import { ThemeProvider } from "@aws-amplify/ui-react";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
